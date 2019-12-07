3611b7048c97b27c45e5e968f7ef52e47f733532
chrisitian.sloper@gmail.com <Christian Sloper>
2019-12-07 22:22:19 +0100

    'working'

--- AoC/intcoder.py
+++ AoC/intcoder.py
@@ -132,42 +132,43 @@ if __name__ == '__main__':
 
     comp = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
     ph = [9,7,8,5,6]
-
-
-    #for ph in permutations(phases):
-
-    names = 'ABCDE'
-
-    
-    # initialize machines:
-    amplifiers = [IntCoder(comp, [p], name ) for p,name in zip(ph,names)]
-
-
-    assert len(amplifiers) == 5
-
-    _input = 0
-
-    
     _mx = 0
 
-    E_output = 0
+    for ph in permutations(ph):
 
-    while not any(a.halted for a in amplifiers):
-    
-        for m in amplifiers:
-            m.add_input(_input)
-            print(m.name, "has inputs", m.inputs )
-            m.produce_output()
-            if m.halted:
-                print(m.name, "halted")
-            else:
+        names = 'ABCDE'
+
+        
+        # initialize machines:
+        amplifiers = [IntCoder(comp, [p], name ) for p,name in zip(ph,names)]
+
+
+        assert len(amplifiers) == 5
+
+        _input = 0
+
+        
+        
+
+        E_output = 0
+
+        while not any(a.halted for a in amplifiers):
+        
+            for m in amplifiers:
+                m.add_input(_input)
+                print(m.name, "has inputs", m.inputs )
+                m.produce_output()
+                # if m.halted:
+                #     print(m.name, "halted")
+                # else:
                 _input = m.popout()
                 print(m.name, " outputs ", _input)
                 if m.name == 'E':
                     E_output = _input
 
-    
-    print(E_output)
+        _mx = max(_mx, E_output)    
+        
+    print(_mx)
 
     # active_machine = 0
     # _input = 0
