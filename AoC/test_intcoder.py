362bbd22eb66ffe62e3f5de84b8f9a36e672b627
chrisitian.sloper@gmail.com <Christian Sloper>
2019-12-08 18:22:31 +0100

    'working'

--- AoC/test_intcoder.py
+++ AoC/test_intcoder.py
@@ -72,7 +72,7 @@ def test_day4_p2_equal_immediate():
 
     assert machine.popout() == 1
 
-def test_day4_p2_equal_immediate():
+def test_day4_p2_not_equal_immediate():
     comp = [3,3,1108,-1,8,3,4,3,99]
     machine = IntCoder(comp, [9])
     machine.intcode()
