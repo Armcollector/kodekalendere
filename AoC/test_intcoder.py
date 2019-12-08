3ca63186573d990bc3ba10c36b9f116f08159905
chrisitian.sloper@gmail.com <Christian Sloper>
2019-12-08 18:24:54 +0100

    'working'

--- AoC/test_intcoder.py
+++ AoC/test_intcoder.py
@@ -95,14 +95,14 @@ def test_jump_test2():
 
 
 def test_jump_test_im():
-    comp =[3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]
+    comp =[3,3,1105,-1,9,1101,0,0,12,4,12,99,1]
     machine = IntCoder(comp, [0])
     machine.intcode()
 
     assert machine.popout() == 0
 
 def test_jump_test2_im():
-    comp =[3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]
+    comp =[3,3,1105,-1,9,1101,0,0,12,4,12,99,1]
     machine = IntCoder(comp, [1])
     machine.intcode()
 
