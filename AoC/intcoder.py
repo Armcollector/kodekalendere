6de414c6d89c371fcc06f4a687a7e996483e7878
Christian Sloper <christian.sloper@gmail.com>
2019-12-09 15:56:16 +0100

    'working'

--- AoC/intcoder.py
+++ AoC/intcoder.py
@@ -37,7 +37,6 @@ class IntCoder:
                 return self.x[self.i+pnr]
             if ps[pnr-1] == 2:
                 return self.x[self.i + pnr]+self.relative_base
-        raise NotImplementedError
 
     def jump_if_true(self,i, ps=[0,0,0]):
         p1 = self.parameter(1,ps)
