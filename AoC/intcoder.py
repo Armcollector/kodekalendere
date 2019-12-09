eb0164f8bdd372c0c68d97f65a194177a134a676
chrisitian.sloper@gmail.com <Christian Sloper>
2019-12-09 22:48:45 +0100

    'working'

--- AoC/intcoder.py
+++ AoC/intcoder.py
@@ -42,10 +42,7 @@ class IntCoder:
         p1 = self.parameter(1,ps)
         p2 = self.parameter(2,ps)
 
-        if p1 != 0:
-            return p2
-        else:
-            return i+3
+        return p2 if p1 else i+3
 
     def adjust_relative(self,i, ps=[0,0,0]):
         p1 = self.parameter(1,ps)
