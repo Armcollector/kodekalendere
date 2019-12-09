8fe75911b696a2c5cc70ce67fdb716882cb80432
chrisitian.sloper@gmail.com <Christian Sloper>
2019-12-09 07:16:04 +0100

    'working'

--- AoC/intcoder.py
+++ AoC/intcoder.py
@@ -19,6 +19,7 @@ class IntCoder:
         self.i = 0
         self.halted = False
         self.name = name
+        self.relative_base=0
 
     def parameter(self, pnr, ps):
         if ps[pnr-1] == 0:
@@ -136,11 +137,9 @@ class IntCoder:
         print(self.name, "receives", _inp)
         self.inputs.append(_inp)
 
+
+def day7():
     
-
-
-if __name__ == '__main__':
-
     comp = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
     comp = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
     ph = [9,8,7,6,5]
@@ -194,3 +193,8 @@ if __name__ == '__main__':
     #         active_machine = (active_machine +1)%5
 
     # print(active_machine, _input) 
+
+
+
+if __name__ == '__main__':
+    pass
