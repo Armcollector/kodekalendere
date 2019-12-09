a77ee7d870bfc49d5bfedb72b464b9377f852d59
chrisitian.sloper@gmail.com <Christian Sloper>
2019-12-09 07:32:12 +0100

    'working'

--- AoC/test_intcoder.py
+++ AoC/test_intcoder.py
@@ -186,3 +186,10 @@ def test_relative2():
     
     machine.intcode()
     assert machine.popout() == 99
+
+def test_relative3():
+    comp= [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+    machine = IntCoder(comp)
+    machine.intcode()
+
+    assert machine.outputs ==  [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
