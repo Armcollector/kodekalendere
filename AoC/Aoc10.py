80e5d6b3074dbb7298cad8035b4c24a2d27c06b1
chrisitian.sloper@gmail.com <Christian Sloper>
2019-12-10 21:14:30 +0100

    'TCRWorking'

--- AoC/Aoc10.py
+++ AoC/Aoc10.py
@@ -21,8 +21,13 @@ def angle_between(v1, v2):
     """
     v1_u = unit_vector(v1)
     v2_u = unit_vector(v2)
-    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
+    if v2[1] < 0:
+        return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0)) + math.pi
+    else:
+        return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
+        
 def select_asteroid(x):
 
 
