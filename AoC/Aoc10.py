18a31a14f7cfa1f94c3a802eba3f92df032249c1
chrisitian.sloper@gmail.com <Christian Sloper>
2019-12-10 20:25:57 +0100

    'TCRWorking'

--- AoC/Aoc10.py
+++ AoC/Aoc10.py
@@ -3,14 +3,25 @@ from fractions import Fraction
 from math import gcd
 import math
 
-def dotproduct(v1, v2):
-  return sum((a*b) for a, b in zip(v1, v2))
+import numpy as np
 
-def length(v):
-  return math.sqrt(dotproduct(v, v))
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.  """
+    return vector / np.linalg.norm(vector)
 
-def cal_angle(v1, v2):
-  return math.acos(dotproduct(v1, v2) / (length(v1) * length(v2)))
+def angle_between(v1, v2):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'::
+
+            >>> angle_between((1, 0, 0), (0, 1, 0))
+            1.5707963267948966
+            >>> angle_between((1, 0, 0), (1, 0, 0))
+            0.0
+            >>> angle_between((1, 0, 0), (-1, 0, 0))
+            3.141592653589793
+    """
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
 def select_asteroid(x):
 
@@ -62,7 +73,7 @@ def laser(x):
         print(f, t)
         
         x,y = diff(f, t)
-        angle = cal_angle((1,0),reduce(x,y))
+        angle = angle_between((1,0),reduce(x,y))
         d[angle] = d.get(angle,[]) + [t]
     print(d)
 
