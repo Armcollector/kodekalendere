7f1923b9702937bc573dbed35e05c09a264ddb67
chrisitian.sloper@gmail.com <Christian Sloper>
2019-12-10 19:43:43 +0100

    'TCRWorking'

--- AoC/Aoc10.py
+++ AoC/Aoc10.py
@@ -12,8 +12,7 @@ def select_asteroid(x):
 
     d = {}            
     for f,t in product(asteroids,repeat=2):
-        x = f[0]-t[0]
-        y = f[1]-t[1]
+        x,y = diff(f, t)
 
         if f == t:
             continue
@@ -55,30 +54,18 @@ def laser(x):
     for t in asteroids:
         print(f, t)
         
-        x = f[0]-t[0]
-        y = f[1]-t[1]
+        x,y = diff(f, t)
 
-        # if f == t:
-        #     continue
-
-        # if gcd(x,y) == 0:
-        #     print(f,t)
-        # g = gcd(x,y)
-        # x//= g
-        # y//= g
-
-
-
-        # if f not in d:
-        #     d[f] = set([])
-
-        # d[f].add((x,y))
-    
+def diff(f, t):
+    x = f[0]-t[0]
+    y = f[1]-t[1]
+    return x,y 
+        
     
 
 
     
-
+ 
 
 
 
