7bf0120c28cd4320c540b7d6010f0291768cc321
Christian Sloper <christian.sloper@gmail.com>
2019-12-11 13:57:12 +0100

    'TCRWorking'

--- AoC/Aoc11.py
+++ AoC/Aoc11.py
@@ -1,5 +1,10 @@
 from .intcoder import IntCoder
-
+from collections import deque
 
 class robot:
-    
+
+    def __init__():
+        self.direction = [(0,1),(1,0),(0,-1),(-1,0)]
+        self.position = (0,0)
+
+        
