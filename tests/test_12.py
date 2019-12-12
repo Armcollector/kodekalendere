11e11e7801dc4718a542d6275b7f7db8b1fa59b5
Christian Sloper <christian.sloper@gmail.com>
2019-12-12 10:10:26 +0100

    'TCRWorking'

--- tests/test_12.py
+++ tests/test_12.py
@@ -12,3 +12,8 @@ def test_parse():
 <x=-9, y=0, z=-4>
 <x=4, y=6, z=0>""")
     assert r == [[-7,17,-11], [9,12,5], [-9,0,-4], [4,6,0]]
+
+
+def test_system():
+    from AoC.Aoc12 import System
+    p = System()
