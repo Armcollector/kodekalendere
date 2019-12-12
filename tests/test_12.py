1415d124b6c6ae9339e7f38e53bf6aaa92d90940
Christian Sloper <christian.sloper@gmail.com>
2019-12-12 10:23:56 +0100

    'TCRWorking'

--- tests/test_12.py
+++ tests/test_12.py
@@ -18,4 +18,7 @@ def test_parse():
 
 def test_system():
     from AoC.Aoc12 import System
-    p = System("x")
+    p = System("x""""<x=-7, y=17, z=-11>
+<x=9, y=12, z=5>
+<x=-9, y=0, z=-4>
+<x=4, y=6, z=0>""")
