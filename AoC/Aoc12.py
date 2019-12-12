06141da846339a55207562d41c809b268cbaf190
Christian Sloper <christian.sloper@gmail.com>
2019-12-12 09:53:13 +0100

    'TCRWorking'

--- AoC/Aoc12.py
+++ AoC/Aoc12.py
@@ -3,3 +3,16 @@ class Planet:
     def __init__(self):
         self.position = None
         self.velocity = None
+
+
+def parse_input(x):
+    pass
+
+if __name__ == '__main__':
+    pass
+
+"""
+    <x=-7, y=17, z=-11>
+    <x=9, y=12, z=5>
+    <x=-9, y=0, z=-4>
+    <x=4, y=6, z=0>"""
