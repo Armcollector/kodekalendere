5f83728fe04545e66d53131c3eb4af365e47d489
chrisitian.sloper@gmail.com <Christian Sloper>
2019-12-14 18:14:33 +0100

    'TCRWorking'

--- AoC/Aoc14.py
+++ AoC/Aoc14.py
@@ -107,8 +107,38 @@ class Factory:
 
 
 
+
+def binarySearch (arr, l, r, x): 
+
+# Check base case 
+if r >= l: 
+
+    mid = l + (r - l)/2
+
+    # If element is present at the middle itself 
+    if arr[mid] == x: 
+        return mid 
+        
+    # If element is smaller than mid, then it  
+    # can only be present in left subarray 
+    elif arr[mid] > x: 
+        return binarySearch(arr, l, mid-1, x) 
+
+    # Else the element can only be present  
+    # in right subarray 
+    else: 
+        return binarySearch(arr, mid + 1, r, x) 
+
+else: 
+    # Element is not present in the array 
+    return -1
+
+
+
 if __name__ == '__main__':
     f = Factory(puzzle_input)
     f.produce("FUEL",10000)
     used, have = f.ore_used, f.have
-    print(used, have)
+    print(used,  10**12-used)
+
+
