dc549e3d22b950e0c2b22e8d9ebb433db15f1b04
Christian Sloper <christian.sloper@gmail.com>
2019-12-16 13:07:50 +0100

    feilaktig forståelse av å snu med seilbåt

--- knowit/Dag16/dag16.py
+++ knowit/Dag16/dag16.py
@@ -19,17 +19,49 @@ for i,c in enumerate(x):
 
 
 
-import PIL
-%matplotlib inline
-import matplotlib.pyplot as plt
-
 a = fjord.reshape(hoyde,-1).transpose()
 
-from PIL import Image
+x,y = np.argwhere(a == 2)[0]
+print(x,y)
 
-im = Image.fromarray(a)
-plt.imshow(im)
+for r in a:
+    for i, c in enumerate(r):
+        if c == 1:
+            continue
+        if i >= 2 and r[i-2] == 1:
+            r[i] = 3
+        if i < len(r)-2 and r[i+2] == 1:
+            r[i] = 4
 
-y,x = np.argwhere(a == 2)[0]
+direction = [-1, 1]
+n = True
+
+#from PIL import Image
+
+#Image.fromarray(a*80).show()
 
 
+s = 1
+for e in range(x+1, len(a)):
+    comment = ''
+    if n:
+        x = e, 
+        y = y-1
+    else:
+        x = e,
+        y = y+1
+    
+    if n and (a[x,y-1] == 3 or a[x,y]==3):
+        n = not n
+        s+=1
+        comment = 'turn 1'
+    elif not n and (a[x, y+1] == 4 or a[x, y] == 4) :
+        s+=1
+        n = not n
+        comment = 'turn 2'
+
+    a[x,y] = 2
+
+    print(e, "".join( [' ', '#', 'N' if n else 'S','3','4'][int(i)] for i in a[x]), comment)
+    
+print(s)
