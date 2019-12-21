b6af6b2ff52f908945682d36d7f1a566bcc299d6
chrisitian.sloper@gmail.com <Christian Sloper>
2019-12-21 12:38:33 +0100

    improve k21

--- knowit/Dag21/knowit21.py
+++ knowit/Dag21/knowit21.py
@@ -1,33 +1,29 @@
+from time import time
+
+start = time()
+
 with open(r"c:\git\kodekalendere\knowit\dag21\generations.txt", 'r') as f:    
     generasjoner = f.read().splitlines()
     generasjoner = [ [  tuple( int(i) for i in a.split(',')) for a in g.split(';') ] for g in  generasjoner  ]
 
+def solve(generasjoner):
 
-antall_alver = len(generasjoner[0])
+    antall_alver = len(generasjoner[0])
+    sets = [set([i]) if i % 2 else set([]) for i in range(antall_alver)]
 
-
-sets = [set([i]) if i % 2 else set([]) for i in range(antall_alver)]
-
-cnt = sum(  1 if i %2 else 0 for i in range(antall_alver) )
-
-print(cnt)
-
-
-for gn, g in enumerate(generasjoner,1):
-    
-    newset = [ set([]) for i in range(antall_alver)]
-    
-    for i,(a,b) in enumerate(g):
-        newset[a] = newset[a].union(sets[i])
+    for gn, g in enumerate(generasjoner,1):
         
-        if len(newset[a]) == cnt:
-            print("found", gn,a)
+        newset = [ set([]) for i in range(antall_alver)]
         
-        newset[b] = newset[b].union(sets[i])
+        for i,(a,b) in enumerate(g):
+            newset[a] = newset[a].union(sets[i])    
+            newset[b] = newset[b].union(sets[i])
 
-        if len(newset[b]) == cnt:
-            print("found", gn,b)
+        for alv, s in enumerate(newset):
+            if len(s) == antall_alver//2:
+                return f"{gn}:{alv} er løsningen."
 
-    sets = newset
+        sets = newset
     
-print(sets)
+print(solve(generasjoner))
+print( time() - start )
