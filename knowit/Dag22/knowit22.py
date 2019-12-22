102ca623583e808c67af3b33c2b63396d7ad6aa4
chrisitian.sloper@gmail.com <Christian Sloper>
2019-12-22 19:11:02 +0100

    numpyversjon av k22

--- knowit/Dag22/knowit22.py
+++ knowit/Dag22/knowit22.py
@@ -1,28 +1,12 @@
 import numpy as np
+from scipy.signal import find_peaks
+
 
 with open(r"C:\git\kodekalendere\knowit\dag22\forest.txt", "r") as f:
     x = f.read()
 
 antall, hoyde = len(x), len(x.splitlines())
-forest = np.zeros( antall )
 
-for i,c in enumerate(x):
-    if c == '#':
-        forest[i]= 1
-    else: 
-        forest[i] = 0
-
-r = forest.reshape(hoyde,-1).transpose()
-
-_mx = 0
-top = []
-for c in r:
-    for i, t in enumerate(c):
-        if t == 1:
-            _mx = max(_mx, r.shape[1] - i)
-            break
-    else:
-        top.append(_mx)
-        _mx = 0
-
-sum([int(i*0.2*200) for i in top])
+r = np.array(list(x.replace(' ','0').replace('#','1').replace('\n','')),dtype=int).reshape(hoyde,-1).transpose()
+r2 = r.shape[1]- np.argmax(r[~np.all(r == 0,axis = 1)],axis=1)
+print(np.sum(r2[find_peaks(r2)[0]]*40))
